155cce73d4d1d6df02eb909e2a646a4d68c819d6
Harry Hallows <hhallows96@gmail.com>
2021-06-12 11:36:53 +0100

    drag and drop

---
+++ JoinedTogetherProject/Assets/Scripts/Crafting/CraftMove.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CraftMove : MonoBehaviour, IPointerDownHandler, IDragHandler, IBeginDragHandler,IEndDragHandler
+{
+
+
+    [SerializeField] private Canvas canvas;
+    private CanvasGroup canvasGroup;
+
+    private RectTransform objectPosition;
+
+    private void Awake()
+    {
+        objectPosition = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("BeginDrag");
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Dragging " + gameObject.name);
+
+        objectPosition.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("Finished Dragging");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Down on " + gameObject.name);
+
+    }
+
+}
---
+++ JoinedTogetherProject/Assets/Scripts/Crafting/CraftPot.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CraftPot : MonoBehaviour, IDropHandler
+{
+
+    public List<GameObject> cookingIngredients = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("DROPPED ON ME");
+
+
+        if (eventData.pointerDrag != null)
+        {
+            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+
+            eventData.pointerDrag.GetComponent<GameObject>() += cookingPot;
+        }
+    }
+}
